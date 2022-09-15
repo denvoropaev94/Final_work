c6b192a512168295431126bdd46186e37bcca325
«Denis» <denvoropaev7@gmail.com>
2022-09-15 16:31:51 +0300

    Решение задачи из итоговой проверочной работы, нахождение строк массива,длины,которых меньше или равны 3, отформатировал код

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,33 +1,38 @@
-﻿string[] array = new string[3];  
+﻿string[] array = new string[3];
 
-  string[] FillArray(string[] arr){
-  for(int i = 0;i<arr.Length;i++)
+string[] FillArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
     {
         Console.Write($"Введите элемент массива под индексом {i}:\t ");
-                arr[i] = Console.ReadLine();
+        arr[i] = Console.ReadLine();
     }
     return arr;
-  }
-    void PrintArray(string[] col){
+}
+void PrintArray(string[] col)
+{
 
     Console.WriteLine("Вывод массива:");
-    for (int i = 0;i<col.Length;i++){
+    for (int i = 0; i < col.Length; i++)
+    {
         Console.Write($"{col[i]} ");
     }
-     Console.WriteLine();
-  }
-string[] FindElementsArray(string[] arr){
+    Console.WriteLine();
+}
+string[] FindElementsArray(string[] arr)
+{
     string[] result = new string[arr.Length];
     for (int i = 0; i < arr.Length; i++)
     {
-       if(arr[i].Length<=3){
-        result[i]=arr[i];
-       }
+        if (arr[i].Length <= 3)
+        {
+            result[i] = arr[i];
+        }
     }
     return result;
-}  
-string[] n  = FillArray(array);
-string result = string.Join(",",n);
+}
+string[] n = FillArray(array);
+string result = string.Join(",", n);
 Console.WriteLine(result);
 string[] res = FindElementsArray(array);
 PrintArray(res);
